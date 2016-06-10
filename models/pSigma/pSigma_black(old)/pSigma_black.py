e2736aca3b930eb72692b8a51a180b6999d5c5d6
Yihong Gu <wmyw96@126.com>
2016-06-10 18:24:32 +0800

    updated

---
+++ models/pSigma/pSigma_black(old)/pSigma_black.py
@@ -0,0 +1,108 @@
+import tensorflow as tf
+import numpy as np
+
+def random_value(shape):
+	return tf.random_normal(shape = shape, mean = 0, stddev = 0.05, dtype = tf.float32)
+def weight_variable(shape):
+	return tf.Variable(random_value(shape), name = 'weight')
+def biases_variable(shape):
+	return tf.Variable(tf.constant(0.1, shape = shape), name = 'biases')
+def conv2d(x, W):
+	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def nn_layer(batch_size, input_tensor, weight_shape, num_filter, layer_name, act = tf.nn.relu, nd_reshape = False):
+	with tf.name_scope(layer_name):
+		with tf.name_scope('weights'):
+			weights = weight_variable(weight_shape);
+		with tf.name_scope('biases'):
+			biases = biases_variable([num_filter])
+		with tf.name_scope('conv_combination'):
+			z = conv2d(input_tensor, weights) + biases
+		with tf.name_scope('activation'):
+			if nd_reshape:
+				reshape = tf.reshape(z, [batch_size, 15 * 15])
+				# a = tf.nn.dropout(act(reshape), 0.8)
+				a = act(reshape)
+			else:
+				a = act(z)
+		return a
+
+def read_data(filename):
+	fp = open(filename);
+	go = []
+	for linea in fp.readlines():
+		go.append(int(linea.replace("\n","")))
+
+	board = np.zeros((15, 15))
+	feature = np.zeros((15, 15, 5))
+	data_state = np.zeros((1, 15, 15, 5))
+	turn = 1
+
+	tot = 0
+
+	for i in range(len(go) / 2):
+		x = go[2 * i]
+		y = go[2 * i + 1]
+		board[x - 1][y - 1] = turn
+		turn = 3 - turn
+
+	for i in range(15):
+		for j in range(15):
+			feature[i, j, 0] = 0
+			feature[i, j, 1] = 1
+			if (abs(board[i, j] - turn) < 1e-9):
+				feature[i, j, 2] = 1
+			else:
+				feature[i, j, 2] = 0
+
+			if (abs(board[i, j] - (3 - turn)) < 1e-9):
+				feature[i, j, 3] = 1
+			else:
+				feature[i, j, 3] = 0
+
+			if (abs(board[i, j]) < 1e-9):
+				feature[i, j, 4] = 1
+			else:
+				feature[i, j, 4] = 0
+	data_state[0] = feature
+	return data_state
+
+def player(filename_input, filename_output):
+	data = read_data(filename_input)
+
+	# play
+	y_target, pos = sess.run([Y, POS], feed_dict={X: data})
+
+	# write the distribution
+	dis_file_object = open(filename_output, 'w+')
+	for i in range(15 * 15):
+		dis_file_object.write(str(y_target[0, i]) + '\n')
+	dis_file_object.close()
+
+	# return the answer (for demo)
+	xx = pos[0] / 15
+	yy = pos[0] % 15
+	return xx, yy, y_target
+
+with tf.name_scope('input'):
+	x = tf.placeholder(tf.float32, [1, 15, 15, 5], 'input_layer')
+
+# neural network
+hidden_conv_layer1 = nn_layer(1, x, [5, 5, 5, 192], 192, 'conv_layer_1')
+hidden_conv_layer2 = nn_layer(1, hidden_conv_layer1, [3, 3, 192, 24], 24, 'conv_layer_2')
+hidden_conv_layer3 = nn_layer(1, hidden_conv_layer2, [3, 3, 24, 24], 24, 'conv_layer_3')
+hidden_conv_layer4 = nn_layer(1, hidden_conv_layer3, [3, 3, 24, 24], 24, 'conv_layer_4')
+hidden_conv_layer5 = nn_layer(1, hidden_conv_layer4, [3, 3, 24, 24], 24, 'conv_layer_5')
+hidden_conv_layer6 = nn_layer(1, hidden_conv_layer5, [3, 3, 24, 24], 24, 'conv_layer_6')
+y = nn_layer(1, hidden_conv_layer6, [1, 1, 24, 1], 1, 'output_layer', tf.nn.softmax, True)
+v = tf.argmax(y, 1);
+saver = tf.train.Saver()
+init = tf.initialize_all_variables()
+
+X = x
+Y = y
+POS = v
+
+sess = tf.InteractiveSession()
+sess.run(init)
+saver.restore(sess, 'tmp/pSigma_black.ckpt')
--- models/pSigma/pSigma_white/pSigma_white_train.py
+++ models/pSigma/pSigma_white/pSigma_white_train.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 def random_value(shape):
-	return tf.random_normal(shape = shape, mean = 0, stddev = 0.05, dtype = tf.float32)
+	return tf.random_normal(shape = shape, mean = 0, stddev = 0.01, dtype = tf.float32)
 def weight_variable(shape):
 	return tf.Variable(random_value(shape), name = 'weight')
 def biases_variable(shape):
@@ -14,8 +14,10 @@ def nn_layer(batch_size, input_tensor, weight_shape, num_filter, layer_name, act
 	with tf.name_scope(layer_name):
 		with tf.name_scope('weights'):
 			weights = weight_variable(weight_shape);
+			variable_set[weights.name] = weights
 		with tf.name_scope('biases'):
 			biases = biases_variable([num_filter])
+			variable_set[biases.name] = biases
 		with tf.name_scope('conv_combination'):
 			z = conv2d(input_tensor, weights) + biases
 		with tf.name_scope('activation'):
@@ -69,52 +71,65 @@ def training(train_size, test_size, cv_size, training_batch_size, training_rate)
 	order = 'o'
 	'''while ((order != 'y') & (order != 'N')):
 		order = raw_input('Continue the train last time? [y/N]')'''
-	order = 'y'
+	order = 'N'
 
-	train_step = tf.train.AdamOptimizer(training_rate).minimize(loss)
-	saver = tf.train.Saver()
-	init = tf.initialize_all_variables()
-	merged = tf.merge_all_summaries()
+	rec = []
+	for k in range(20):
+		training_rate = training_rate / 2.0;
+		saver = tf.train.Saver(variable_set)
+		train_step = tf.train.AdamOptimizer(training_rate).minimize(loss)
+		init = tf.initialize_all_variables()
+		merged = tf.merge_all_summaries()
+		
+		max_acc = 0
 
-	max_acc = 0
+		# train
+		with tf.Session() as sess:
+			train_writer = tf.train.SummaryWriter('tmp/pSigma_white/train', sess.graph)
+			test_writer = tf.train.SummaryWriter('tmp/pSigma_white/test', sess.graph)
 
-	# train
-	with tf.Session() as sess:
-		train_writer = tf.train.SummaryWriter('tmp/pSigma_white/train', sess.graph)
-		test_writer = tf.train.SummaryWriter('tmp/pSigma_white/test', sess.graph)
-		sess.run(init)
-		if (order == 'y'):
-			saver.restore(sess, 'tmp/pSigma_white.ckpt')
+			sess.run(init)
+			# if (order == 'y'):
+			if (k > 0):
+				saver.restore(sess, 'tmp/pSigma_white.ckpt')
 
-		for i in range(50000):
-			if (i % 100 == 0):
-				sacc = 0
-				slos = 0
-				for j in range(cv_size / training_batch_size):
-					batch_xs, batch_ys = gomoku.crossvalidation.next_batch(training_batch_size)
-					summary, acc, los = sess.run([merged, accuracy, loss], feed_dict={x: batch_xs, y_: batch_ys})
-					sacc = sacc + acc
-					slos = slos + los
+			for i in range(10001):
+				if (i % 100 == 0):
+					sacc = 0
+					slos = 0
 
-				av_acc = sacc / (cv_size / training_batch_size)
-				av_los = slos / (cv_size / training_batch_size)
+					for j in range(cv_size / training_batch_size):
+						batch_xs, batch_ys = gomoku.crossvalidation.next_batch(training_batch_size)
+						summary, acc, los = sess.run([merged, accuracy, loss], feed_dict={x: batch_xs, y_: batch_ys})
+						sacc = sacc + acc
+						slos = slos + los
 
-				test_writer.add_summary(summary, i)
-				#if ((i % 250 == 0) | ((i % 50 == 0) & (i < 250))):
-				print 'Step %s: Accuracy(%s), Loss(%s)' % (i, av_acc, av_los)
-				if (sacc > max_acc):
-					max_acc = acc
-				save_path = saver.save(sess, 'tmp/pSigma_white.ckpt')
-				# print 'successfully saved in path', save_path
-			else: 
-				batch_xs, batch_ys = gomoku.train.next_batch(training_batch_size)
-				summary, _, yt, lossv  = sess.run([merged, train_step, y, loss], feed_dict={x: batch_xs, y_: batch_ys})
-				train_writer.add_summary(summary, i)
-				# print 'Loss at step %s : %s' % (i, lossv)
+					av_acc = sacc / (cv_size / training_batch_size)
+					av_los = slos / (cv_size / training_batch_size)
 
-		#print 'The final accuracy of test set'
-		#print sess.run(accuracy, feed_dict={x: gomoku.test.state, y_: gomoku.test.action})
+					rec.append([av_acc, av_los])
 
-# training(492, 100, 100, 1, 0.003)
+					test_writer.add_summary(summary, i)
+					#if ((i % 250 == 0) | ((i % 50 == 0) & (i < 250))):
+					print 'Step %s: Accuracy(%s), Loss(%s)' % (i + k * 10000, av_acc, av_los)
+					if (sacc > max_acc):
+						max_acc = acc
+					save_path = saver.save(sess, 'tmp/pSigma_white.ckpt')
+					# print 'successfully saved in path', save_path
+
+					file_ob = open('traing.txt', 'w+')
+					for j in range(len(rec)):
+						file_ob.write('Step %s: Accuracy(%s), Loss(%s)\n' % (j * 100, rec[j][0], rec[j][1]))
+					file_ob.close()
+				else:
+					batch_xs, batch_ys = gomoku.train.next_batch(training_batch_size)
+					summary, _, yt, lossv  = sess.run([merged, train_step, y, loss], feed_dict={x: batch_xs, y_: batch_ys})
+					train_writer.add_summary(summary, i)
+					# print 'Loss at step %s : %s' % (i, lossv)
+
+			#print 'The final accuracy of test set'
+			#print sess.run(accuracy, feed_dict={x: gomoku.test.state, y_: gomoku.test.action})
+
+variable_set = {}
 training(218853, 60000, 60000, 100, 0.0003)
 
